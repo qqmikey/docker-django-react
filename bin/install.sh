1946d8caf984f03abb488a1bcc43c7511861e17c
qqmikey <qqmikey@icloud.com>
2021-04-01 14:24:43 -0500

    Update install.sh

--- bin/install.sh
+++ bin/install.sh
@@ -31,7 +31,7 @@ if [ `lsb_release -s -i` == 'Ubuntu' ]; then
 
     check_package docker-ce
 
-    sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+    sudo curl -L https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
     sudo chmod +x /usr/local/bin/docker-compose
 
 fi
